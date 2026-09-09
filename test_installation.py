73f4de9af2f5240472d1680aa0be5fcb8f277dd7
Sudharshini <kannansudharshini@gmail.com>
2026-09-09 15:01:07 +0530

    Initialize single-cell multi-omics project

---
+++ test_installation.py
@@ -0,0 +1,9 @@
+import scanpy as sc
+import muon as mu
+import mudata as md
+
+print("Scanpy version:", sc.__version__)
+print("Muon version:", mu.__version__)
+print("MuData version:", md.__version__)
+
+print("\nAll packages installed successfully!")
